5519c0fb20b0a7855ff4de28bc5d65983966945f
Jose Carlos <jescuderv@alumnos.unex.es>
2016-11-16 01:22:53 +0100

    + UI: StatisticsFragment-> Implementación prueba en java y xml. + UI: FriendsFragment-> Arreglo Podium: adaptado a la pantalla con cada uno de los personajes.
    ///////////////////////////////////////////////////////////////
    ********** IMPLEMENTACIÓN ACTUAL ***************
    //////////////////////////////////////////////////////////////
    
    +UI: LoginActivity-> Implementación sin funcionalidad en java y xml.
    +UI: RegisterActivity-> Implementación sin funcinalidad en java y xml.
     +UI: ProfileUserActivity-> Implementación sin funcionalidad en java y xml.
    +UI: NavigationDrawer-> Implementación java y xml. Falta: iconos a mostrar y funcionalidad en el Header. Menu opciones sin funcionalidad.
    +UI: WeightControlFragment-> Implementación sin funcinoalidad en java y xml.
    +UI: FriendsFragment-> Implementación prueba en java y xml.
    +UI:CalendarFragment-> Implementación prueba en java y xml.

--- app/src/main/java/asee/giiis/unex/es/mysporttraining/Fragments/StatisticsFragment.java
+++ app/src/main/java/asee/giiis/unex/es/mysporttraining/Fragments/StatisticsFragment.java
@@ -6,6 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import asee.giiis.unex.es.mysporttraining.R;
 
 
@@ -14,6 +20,55 @@ public class StatisticsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+
+        GraphView graph = (GraphView) view.findViewById(R.id.graphic_score);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6),
+                new DataPoint(5, 1),
+                new DataPoint(6, 5),
+                new DataPoint(7, 3),
+                new DataPoint(8, 2),
+                new DataPoint(9, 6),
+                new DataPoint(10, 1),
+                new DataPoint(11, 5),
+                new DataPoint(12, 3),
+                new DataPoint(13, 2),
+                new DataPoint(14, 6)
+        });
+        graph.addSeries(series);
+
+        GridLabelRenderer gridLabel = graph.getGridLabelRenderer();
+        gridLabel.setVerticalAxisTitle("PUNTUACIÓN");
+
+        // Max labels in X Axis
+        gridLabel.setNumHorizontalLabels(3);
+
+        // activate horizontal zooming and scrolling
+        graph.getViewport().setScalable(true);
+
+        // activate horizontal scrolling
+        graph.getViewport().setScrollable(true);
+
+        // custom label formatter
+        gridLabel.setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    // show normal x values
+                    return super.formatLabel(value, isValueX)  + " fecha";
+                } else {
+                    // show currency for y values
+                    return super.formatLabel(value, isValueX);
+                }
+            }
+        });
+
+
+        return view;
     }
 }
