b4ac5c1f97770d5d8d87f91a00c1a1b2d1b6f397
Jose Carlos <jescuderv@alumnos.unex.es>
2016-11-16 17:13:48 +0100

    + UI: StatisticsFragment-> Implementación prueba en java y xml. + UI: FriendsFragment-> Arreglo Podium: adaptado a la pantalla con cada uno de los personajes.
    ///////////////////////////////////////////////////////////////
    ********** IMPLEMENTACIÓN ACTUAL ***************
    //////////////////////////////////////////////////////////////
    
    +UI: LoginActivity-> Implementación sin funcionalidad en java y xml.
    +UI: RegisterActivity-> Implementación sin funcinalidad en java y xml.
     +UI: ProfileUserActivity-> Implementación sin funcionalidad en java y xml.
    +UI: NavigationDrawer-> Implementación java y xml. Falta: iconos a mostrar y funcionalidad en el Header. Menu opciones sin funcionalidad.
    +UI: WeightControlFragment-> Implementación sin funcinoalidad en java y xml.
    +UI: FriendsFragment-> Implementación prueba en java y xml.
    +UI:CalendarFragment-> Implementación prueba en java y xml.

--- app/src/main/java/asee/giiis/unex/es/mysporttraining/Fragments/ActivityFragment.java
+++ app/src/main/java/asee/giiis/unex/es/mysporttraining/Fragments/ActivityFragment.java
@@ -3,23 +3,45 @@ package asee.giiis.unex.es.mysporttraining.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import asee.giiis.unex.es.mysporttraining.R;
 
-public class ActivityFragment extends Fragment {
+public class ActivityFragment extends Fragment implements OnMapReadyCallback {
 
-    public ActivityFragment() {
-        // Required empty public constructor
-    }
+    private GoogleMap mGoogleMap;
+    private SupportMapFragment supportMapFragment;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activity, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_activity, container, false);
+
+        FragmentManager fm = getChildFragmentManager();
+        supportMapFragment = (SupportMapFragment) fm.findFragmentById(R.id.google_map);
+        if (supportMapFragment == null) {
+            supportMapFragment = SupportMapFragment.newInstance();
+            fm.beginTransaction().replace(R.id.google_map, supportMapFragment).commit();
+        }
+
+        supportMapFragment.getMapAsync(this);
+
+        return view;
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
     }
 
 }
--- app/src/main/java/asee/giiis/unex/es/mysporttraining/Fragments/StatisticsFragment.java
+++ app/src/main/java/asee/giiis/unex/es/mysporttraining/Fragments/StatisticsFragment.java
@@ -68,7 +68,6 @@ public class StatisticsFragment extends Fragment {
             }
         });
 
-
         return view;
     }
 }
