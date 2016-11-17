9234107271bc7f25dccc92c97787a1250f1a3005
Jose Carlos <jescuderv@alumnos.unex.es>
2016-11-17 18:49:50 +0100

    Cambios v0.4.1: +UI: RegisterActivity-> Agregados spinners de sexo y condición física. +Implementación UI básica finalizada.
    ///////////////////////////////////////////////////////////////
    ********** IMPLEMENTACIÓN ACTUAL ***************
    //////////////////////////////////////////////////////////////
    +UI: ActivityFragment-> Implemntación sin funcionaliad muy básica.
    +UI: ResultsMapActivity-> Implementación de un mapa google maps e inicialización, sin funcionalidad.
    + UI: StatisticsFragment-> Implementación prueba en java y xml.
    + UI: FriendsFragment-> Arreglo Podium: adaptado a la pantalla con cada uno de los personajes.
    +UI: LoginActivity-> Implementación sin funcionalidad en java y xml.
    +UI: RegisterActivity-> Implementación sin funcinalidad en java y xml.
     +UI: ProfileUserActivity-> Implementación sin funcionalidad en java y xml.
    +UI: NavigationDrawer-> Implementación java y xml. Falta: iconos a mostrar y funcionalidad en el Header. Menu opciones sin funcionalidad.
    +UI: WeightControlFragment-> Implementación sin funcinoalidad en java y xml.
    +UI: FriendsFragment-> Implementación prueba en java y xml.
    +UI:CalendarFragment-> Implementación prueba en java y xml.

--- app/src/main/java/asee/giiis/unex/es/mysporttraining/RegisterActivity.java
+++ app/src/main/java/asee/giiis/unex/es/mysporttraining/RegisterActivity.java
@@ -3,16 +3,19 @@ package asee.giiis.unex.es.mysporttraining;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import java.io.IOException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import fr.ganfra.materialspinner.MaterialSpinner;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -41,7 +44,45 @@ public class RegisterActivity extends AppCompatActivity {
                 startGetImageProfile();
             }
         });
+
+        // Adapter for spinner sex (take array from resources)
+        ArrayAdapter<String> adapterSex = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.sex_array));
+        adapterSex.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        MaterialSpinner spinnerSex = (MaterialSpinner) findViewById(R.id.reg_spinner_sex);
+        spinnerSex.setAdapter(adapterSex);
+        spinnerSex.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        // Adapter for spinner weight condition (take array from resources)
+        ArrayAdapter<String> adapterWeightCond = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.weight_condition_array));
+        adapterWeightCond.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        MaterialSpinner spinnerWeightCond = (MaterialSpinner) findViewById(R.id.reg_spinner_weight_conditino);
+        spinnerWeightCond.setAdapter(adapterWeightCond);
+        spinnerWeightCond.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
+
 
     private void startLoginActivity(){
         Intent intent = new Intent(this, LoginActivity.class);
@@ -70,4 +111,6 @@ public class RegisterActivity extends AppCompatActivity {
             }
         }
     }
+
+
 }
