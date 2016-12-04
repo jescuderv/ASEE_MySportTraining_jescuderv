b21167a2e8c66c2b325a59bbd37b4a04b8a122e2
Jose Carlos <jescuderv@alumnos.unex.es>
2016-12-04 20:45:36 +0100

    Cambios v0.7 +Registro de un usuario nuevo en la base de datos Firebase (RegisterActivity)
    ///////////////////////////////////////////////////////////////
    ********** IMPLEMENTACIÓN ACTUAL ***************
    //////////////////////////////////////////////////////////////
    
    Cambios v0.6.6
    +Paso por intents el nombre del entrenamiento a realizar para poder recuperar y crear en la base de datos (TrainingFragment, TrainingNewActivyt, TrainingSelectCategoryActivity, TrainingSelectActivity).
    
    Cambios v0.6.5
    +TrainingFragment: agregado dialog para insertar nombre de entrenamiento.
    
    Cambios v0.6.4:
    +TrainingFragment: se recupera d Firebase todos los entrenamientos creados.
    
    Cambios v0.6.3:
    +TrainingNewActivity: se recuperan de Firebase los ejercicios agregados en TrainingSelectActivity. Se muestran los detalles de cada ejercicio al pulsar sobre cada uno.
    
    Cambios v0.6.2:
    +Se agrega a la base de datos Firebase un ejercicio al especificar la hora y fecha (se agrega: nombre, score, hora, fecha), en TrainingSelectActivity.
    
    Cambios v0.6.1:
    +Cuando se pulse sobre un ejercicio de la lista en TrainingSelectActivity, se abrirá un diálogo (definido en dialog_input_exercise.xml), que permitirá seleccionar fecha y hora para el ejercicio.
    +Implementado pickers para fecha y hora del diálogo anterior.
    
    Cambios v0.6:
     +Conexion a firebase en TrainingSelectCategoryAc para recuperar lista de categorias.
    +Conexion a firebase en TrainingSelectActivity para recuperar lista de ejercicios.
    
    Cambios v0.5.2:
    +Agregados varios Activity para el apartado de entrenamiento, selección de ejercicios y selección de la categoría, además de la clase POJO Activity(Actividad).
    
    Cambios v0.5.1:
    +Agregados varios Acitivy para el apartado de Entrenamiento y AdapterTraining
    
    Cambios v0.5:
    +Firebase-> UserProfileActivity. Conexión a la base de datos y muestra del perfil de usuario.
    
    Cambios v0.4.1:
    +UI: RegisterActivity-> Agregados spinners de sexo y condición física.
    +Implementación UI básica finalizada.
    
    Cambios v0.4:
    +UI: ActivityFragment-> Implemntación sin funcionaliad muy básica.
    +UI: ResultsMapActivity-> Implementación de un mapa google maps e inicialización, sin funcionalidad.
    
    Cambios v0.1:
    + UI: StatisticsFragment-> Implementación prueba en java y xml.
    + UI: FriendsFragment-> Arreglo Podium: adaptado a la pantalla con cada uno de los personajes.
    +UI: LoginActivity-> Implementación sin funcionalidad en java y xml.
    +UI: RegisterActivity-> Implementación sin funcinalidad en java y xml.
     +UI: ProfileUserActivity-> Implementación sin funcionalidad en java y xml.
    +UI: NavigationDrawer-> Implementación java y xml. Falta: iconos a mostrar y funcionalidad en el Header. Menu opciones sin funcionalidad.
    +UI: WeightControlFragment-> Implementación sin funcinoalidad en java y xml.
    +UI: FriendsFragment-> Implementación prueba en java y xml.
    +UI:CalendarFragment-> Implementación prueba en java y xml.

--- app/src/main/java/asee/giiis/unex/es/mysporttraining/Objects/User.java
+++ app/src/main/java/asee/giiis/unex/es/mysporttraining/Objects/User.java
@@ -3,23 +3,6 @@ package asee.giiis.unex.es.mysporttraining.Objects;
 
 public class User {
 
-//    private int id;
-//    private String imageProfile; // source
-//    private String username;
-//    private String firstName;
-//    private String lastName;
-//    private String email;
-//    private String password;
-//    private Integer height;
-//    private Integer weight;
-//    private Integer age;
-//    private String sex;
-//    private String weightCondition;
-//    private String score;
-//    private List<User> friendList;
-//    private Map<Date, String> weight_control;
-//    private List<Activity> activityList;
-
     private Integer age;
     private String email;
     private String firstName;
@@ -36,6 +19,18 @@ public class User {
 
     }
 
+    public User(Integer age, String email, String firstName, Integer height, String lastName,
+                String password, Integer score, String username, Integer weight) {
+        this.age = age;
+        this.email = email;
+        this.firstName = firstName;
+        this.height = height;
+        this.lastName = lastName;
+        this.password = password;
+        this.score = score;
+        this.username = username;
+        this.weight = weight;
+    }
 
     public Integer getAge() {
         return age;
@@ -128,133 +123,3 @@ public class User {
 
 
 }
-//
-//    public User(String username, String firstName, String lastName, String score) {
-//        this.username = username;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.score = score;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public Map<Date, String> getWeight_control() {
-//        return weight_control;
-//    }
-//
-//    public void setWeight_control(Map<Date, String> weight_control) {
-//        this.weight_control = weight_control;
-//    }
-//
-//
-//    public String getImageProfile() {
-//        return imageProfile;
-//    }
-//
-//    public void setImageProfile(String imageProfile) {
-//        this.imageProfile = imageProfile;
-//    }
-//
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public Integer getHeight() {
-//        return height;
-//    }
-//
-//    public void setHeight(Integer height) {
-//        this.height = height;
-//    }
-//
-//    public Integer getWeight() {
-//        return weight;
-//    }
-//
-//    public void setWeight(Integer weight) {
-//        this.weight = weight;
-//    }
-//
-//    public Integer getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(Integer age) {
-//        this.age = age;
-//    }
-//
-//    public String getSex() {
-//        return sex;
-//    }
-//
-//    public void setSex(String sex) {
-//        this.sex = sex;
-//    }
-//
-//    public String getWeightCondition() {
-//        return weightCondition;
-//    }
-//
-//    public void setWeightCondition(String weightCondition) {
-//        this.weightCondition = weightCondition;
-//    }
-//
-//    public String getScore() {
-//        return score;
-//    }
-//
-//    public void setScore(String score) {
-//        this.score = score;
-//    }
-//
-//    public List<User> getFriendList() {
-//        return friendList;
-//    }
-//
-//    public void setFriendList(List<User> friendList) {
-//        this.friendList = friendList;
-//    }
-//}
--- app/src/main/java/asee/giiis/unex/es/mysporttraining/RegisterActivity.java
+++ app/src/main/java/asee/giiis/unex/es/mysporttraining/RegisterActivity.java
@@ -5,28 +5,40 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import asee.giiis.unex.es.mysporttraining.Objects.User;
 import de.hdodenhof.circleimageview.CircleImageView;
 import fr.ganfra.materialspinner.MaterialSpinner;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    static private final int REQUEST_IMAGE_GET = 1;
+    private static final int REQUEST_IMAGE_GET = 1;
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9#_~!$&'()*+,;=:.\"(),:;<>@\\[\\]\\\\]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*$";
+    private static final String NAME_PATTERN = "[^0-9]+";
+
+    String mCond, mSex;
+
+    // Our own user
+    private User mUser;
 
     // Reference root JSON database
-    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference mUsersRef = mRootRef.child("users");
+    private DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference mUsersRef = mRootRef.child("users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +48,8 @@ public class RegisterActivity extends AppCompatActivity {
         // Focus for input disabled
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
-        Button registerButton = (Button) findViewById(R.id.reg_register_button);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startLoginActivity();
-            }
-        });
+
+        // ===== IMAGE PROFILE ==== //
 
         CircleImageView imageProfile = (CircleImageView) findViewById(R.id.reg_usr_profile_image);
         imageProfile.setOnClickListener(new View.OnClickListener() {
@@ -52,39 +59,61 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+
+        // ===== SPINNERS ==== //
+
         // Adapter for spinner sex (take array from resources)
         ArrayAdapter<String> adapterSex = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
                 getResources().getStringArray(R.array.sex_array));
         adapterSex.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         MaterialSpinner spinnerSex = (MaterialSpinner) findViewById(R.id.reg_spinner_sex);
         spinnerSex.setAdapter(adapterSex);
-        spinnerSex.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
         // Adapter for spinner weight condition (take array from resources)
         ArrayAdapter<String> adapterWeightCond = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
                 getResources().getStringArray(R.array.weight_condition_array));
         adapterWeightCond.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        MaterialSpinner spinnerWeightCond = (MaterialSpinner) findViewById(R.id.reg_spinner_weight_conditino);
+        MaterialSpinner spinnerWeightCond = (MaterialSpinner) findViewById(R.id.reg_spinner_weight_condition);
         spinnerWeightCond.setAdapter(adapterWeightCond);
+
         spinnerWeightCond.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mCond = parent.getItemAtPosition(position).toString();
 
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(parent.getContext(), "Faltan algunos campos por rellenar", Toast.LENGTH_SHORT);
+            }
+        });
+        spinnerSex.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mSex = parent.getItemAtPosition(position).toString();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(parent.getContext(), "Faltan algunos campos por rellenar", Toast.LENGTH_SHORT);
+            }
+        });
+
+
+
+
+
+
+        // ===== REGISTER BUTTON ==== //
+
+        Button registerButton = (Button) findViewById(R.id.reg_register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Register user in database
+                registerUser();
             }
         });
 
@@ -95,6 +124,176 @@ public class RegisterActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
+
+
+    //========================================//
+        // REGISTER AN NEW USER IN FIREBASE //
+    //========================================//
+
+
+    private void registerUser(){
+
+        boolean error = false;
+
+        // Get from Activity
+        TextInputLayout username = (TextInputLayout) findViewById(R.id.reg_input_layout_username);
+        String usrUsername = username.getEditText().getText().toString();
+
+        TextInputLayout firstname = (TextInputLayout) findViewById(R.id.reg_input_layout_first_name);
+        String usrFirstname = firstname.getEditText().getText().toString();
+
+        TextInputLayout lastname = (TextInputLayout) findViewById(R.id.reg_input_layout_last_name);
+        String usrLastname = lastname.getEditText().getText().toString();
+
+        TextInputLayout email = (TextInputLayout) findViewById(R.id.reg_input_layout_email);
+        String usrEmail = email.getEditText().getText().toString();
+
+        TextInputLayout password = (TextInputLayout) findViewById(R.id.reg_input_layout_password);
+        String usrPasswrod = password.getEditText().getText().toString();
+
+        TextInputLayout height = (TextInputLayout) findViewById(R.id.reg_input_layout_height);
+        String usrHeight = height.getEditText().getText().toString();
+
+        TextInputLayout weight = (TextInputLayout) findViewById(R.id.reg_input_layout_weight);
+        String usrWeight = weight.getEditText().getText().toString();
+
+        TextInputLayout age = (TextInputLayout) findViewById(R.id.reg_input_layout_age);
+        String usrAge = age.getEditText().getText().toString();
+
+
+        // Check null
+        if (usrUsername.isEmpty() || usrFirstname.isEmpty() || usrLastname.isEmpty() ||
+                usrEmail.isEmpty() || usrPasswrod.isEmpty() || usrHeight == null
+                || usrWeight == null || usrAge == null || mSex.equals("Sexo") || mCond.equals("Condición física")){
+            error = true;
+            Toast.makeText(this, "Faltan algunos campos por rellenar", Toast.LENGTH_SHORT).show();
+        }
+
+        // Check syntax
+        if (!error){
+            error = false;
+            // username
+            if (!checkLenght(usrUsername)){
+                username.setErrorEnabled(true);
+                username.setError("Introduce un nombre de usuario de al menos 6 caracteres");
+                error = true;
+            } else { username.setErrorEnabled(false); }
+            // first name
+            if (!checkName(usrFirstname)) {
+                firstname.setErrorEnabled(true);
+                firstname.setError("Introduce un nombre válido");
+                error = true;
+            } else { firstname.setErrorEnabled(false); }
+            // last name
+            if(!checkName(usrLastname)){
+                lastname.setErrorEnabled(true);
+                lastname.setError("Introduce un apellido válido");
+                error = true;
+            } else { lastname.setErrorEnabled(false); }
+            // email
+            if (!checkEmail(usrEmail)){
+                email.setErrorEnabled(true);
+                email.setError("Introduce un email válido");
+                error = true;
+            } else { email.setErrorEnabled(false); }
+            // password
+            if (!checkLenght(usrPasswrod)){
+                password.setErrorEnabled(true);
+                password.setError("Introduce al menos 5 caracteres");
+                error = true;
+            } else { password.setErrorEnabled(false); }
+            // height
+            if (!checkHeight(usrHeight)){
+                height.setErrorEnabled(true);
+                height.setError("Introduce un valor entre 100 cm y 220 cm");
+                error = true;
+            } else { height.setErrorEnabled(false); }
+            // weight
+            if (!checkWeight(usrWeight)) {
+                weight.setErrorEnabled(true);
+                weight.setError("Introduce un valor válido para el peso");
+                error = true;
+            } else { weight.setErrorEnabled(false); }
+            // age
+            if (!checkAge(usrAge)){
+                age.setErrorEnabled(true);
+                age.setError("Introduce un valor válido para la edad");
+                error = true;
+            } else { age.setErrorEnabled(false); }
+
+        }
+
+
+        // Check Firebase
+
+
+
+        // Add new user to Firebase
+        if (!error){
+            mUser = new User(Integer.parseInt(usrAge), usrEmail, usrFirstname, Integer.parseInt(usrHeight), usrLastname,
+                    usrPasswrod, 0, usrUsername, Integer.parseInt(usrWeight));
+            mUser.setSex(mSex);
+            mUser.setPhysicalCondition(mCond);
+            mUsersRef.push().setValue(mUser);
+            Toast.makeText(this, "Usuario registrado correctamente", Toast.LENGTH_LONG);
+            // Go to Login Activity
+            startLoginActivity();
+        }
+
+    }
+
+    // Check Length for username and password
+    private boolean checkLenght(String username){
+        return username.length() > 5;
+    }
+
+    // Check name for firstname and lastname
+    private boolean checkName(String name){
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        Matcher matcher;
+        matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+
+    // Check email
+    private boolean checkEmail(String email){
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher;
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // Check height
+    private boolean checkHeight(String height){
+        Integer heightAux = Integer.parseInt(height);
+        if (heightAux < 220 && heightAux > 100){
+            return true;
+        }
+        return false;
+    }
+
+    // check weight
+    private boolean checkWeight(String weight){
+        Integer weightAux = Integer.parseInt(weight);
+        if (weightAux < 300 && weightAux > 25){
+            return true;
+        }
+        return false;
+    }
+
+    // check age
+    private boolean checkAge(String age){
+        Integer ageAux = Integer.parseInt(age);
+        if (ageAux < 120 && ageAux > 0){
+            return true;
+        }
+        return false;
+    }
+
+    //========================================//
+            // SET IMAGE PROFILE //
+    //========================================//
 
     private void startGetImageProfile(){
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -118,6 +317,5 @@ public class RegisterActivity extends AppCompatActivity {
             }
         }
     }
-
 
 }
